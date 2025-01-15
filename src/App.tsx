da5d36ca43e26132ed74459f3ead743a130275bd
Manikandan S <mail2aj19@gmail.com>
2025-01-15 20:08:37 +0530

    feat: synced audio play

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,34 @@
 import { useEffect, useState, useRef } from 'react';
 import './App.css'
 import { Peer } from "peerjs";
-// import { io } from 'socket.io-client';
 
 
 
 
 function App() {
   const [peerid, setPeerid] = useState("");
-  const [isPlaying, setIsPlaying] = useState(false);
   const peer = new Peer();
   var context;
   var buf;
   var source;
-  // const byteArray = []
 
   function init() {
-    // if (!window.AudioContext) {
-    // if (!window.webkitAudioContext) {
-    //     alert("Your browser does not support any AudioContext and cannot play back this audio.");
-    //     return;
-    // }
-    //     window.AudioContext = window.webkitAudioContext;
-    // }
-
     context = new AudioContext();
   }
-
 
 
   peer.on("connection", (conn) => {
 
     conn.on("data", (data) => {
       console.log("Data : ", data)
-      // byteArray.push(data)
-      // console.log(typeof(data))
-      if(!isPlaying) playByteArray(data)
+      if(data === "play") {
+        source.start(0)
+      }
+      else if(data instanceof Uint8Array) playByteArray(data)
     });
-
-    // if(byteArray.length > 500) {
-    //   conn.send('Start');
-    // }
   })
 
   function playByteArray(byteArray) {
-    // console.log("Coming here : ", buf)
     try {
       var arrayBuffer = new ArrayBuffer(byteArray.length);
       var bufferView = new Uint8Array(arrayBuffer);
@@ -69,10 +53,7 @@ function App() {
     try {
       source = context.createBufferSource();
       source.buffer = buf;
-      // Connect to the final output node (the speakers
       source.connect(context.destination);
-      // Play immediately
-      // source.start(0);
     }
     catch(e){
       // error handling
@@ -81,12 +62,8 @@ function App() {
   }
 
   async function getAudioStream(audioFilePath) {
-    // const audio = new Audio(audioFilePath);
-    // audio.play()
     const response = await fetch(audioFilePath, {'mode': 'cors'}); // Fetch the audio file
     const buffer = await response.arrayBuffer(); // Convert to ArrayBuffer
-
-    // console.log(buffer)
     const view = new Uint8Array(buffer);
 
     return view;
@@ -96,12 +73,6 @@ function App() {
   function sendStream(audioFilePath) { 
     getAudioStream(audioFilePath).then((buf) => {
       var conn = peer.connect(peerid);
-      // for (let i = 0; i < buf.length; i++) {
-      //   conn.on('open', () => {
-
-      //     conn.send(buf[i]);
-      //   })
-      // }
         conn.on('open', () => {
 
           conn.send(buf);
@@ -110,6 +81,15 @@ function App() {
     .catch(e => {
       console.log("Error Occured : ", e)
       return
+    })
+  }
+
+  function peersPlay() {
+    var conn = peer.connect(peerid);
+    const aud = new Audio('audio.mp3')
+    conn.on('open', () => {
+      conn.send("play");
+      aud.play()
     })
   }
 
@@ -126,7 +106,7 @@ function App() {
     <>
       <input value = {peerid} onChange={e => setPeerid(e.target.value)}></input>
       <button onClick = {() => sendStream("audio.mp3")}>Connect</button>
-      <button onClick={() => {source.start(0)}}>Play</button>
+      <button onClick={() => peersPlay()}>Play</button>
     </>
   )
 }
